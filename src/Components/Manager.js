e71e2be8c4a99e961a1745b9311dd17247986007
linepagter <202105687@post.au.dk>
2024-04-20 18:41:26 +0200

    Manager created

--- src/Components/Manager.js
+++ src/Components/Manager.js
@@ -5,6 +5,31 @@ export function Manager() {
     return (
       <Fragment>
         <h2> Create new manager </h2>
+
+      <div className="react-form">
+      <form className="">
+          <div className="input-div">
+            <label> First Name </label>
+            <input placeholder="First name" autoFocus='true' />
+          </div>
+
+          <div className="input-div">
+            <label> Last Name </label>
+            <input placeholder="Last name" autoFocus='true' />
+          </div>
+
+          <div className="input-div">
+            <label> Email </label>
+            <input placeholder="Email" autoFocus='true' />
+          </div>
+
+          <div className="input-div">
+            <label> Password </label>
+            <input placeholder="Password" autoFocus='true' />
+          </div>
+
+          </form>
+        </div>
       </Fragment>
     )
 }
