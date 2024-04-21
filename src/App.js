6b28920b84a71453451ac6cddcfb901beb1d5474
cthdk <202109149@post.au.dk>
2024-04-21 12:17:03 +0200

    Login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+import Login from './Components/Login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Login/>
       </header>
     </div>
   );
---
+++ src/Components/Login.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+
+export function Login() { 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            const url = 'http://localhost:7181/api/account/login'; 
+            const response = await fetch(url, {
+                method: 'POST',
+                body: JSON.stringify({ email, password }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (response.ok) {
+                const { jwt } = await response.json();
+                localStorage.setItem('token', jwt);
+                // Redirect or do something else after successful login
+            } else {
+                const errorMessage = await response.text();
+                alert('Server returned: ' + errorMessage);
+            }
+        } catch (err) {
+            alert('Error: ' + err);
+        }
+    };
+
+    return (
+        <form onSubmit={handleLogin}>
+            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <input type="password" placeholder="Adgangskode" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            <button type="submit">Log ind</button>
+        </form>
+    );
+};
+
+export default Login;
