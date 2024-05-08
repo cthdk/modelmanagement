6b740a35e3eed1068864bc651b895fec432e3986
cthdk <202109149@post.au.dk>
2024-05-08 15:16:15 +0200

    jobs

--- src/Components/Jobs.js
+++ src/Components/Jobs.js
@@ -2,79 +2,86 @@ import { Fragment, useState, useEffect } from "react";
 import Layout from "./Layout";
 
 export function Jobs() {
-  const [state, setState] = useState();
   const [models, setModels] = useState([]);
-  //const [message, setMessage] = useState("");
+
+  const [jobs, setJobs] = useState({
+    customer: "",
+    startDate: "",
+    days: "",
+    location: "",
+    comments: "",
+    models: [] 
+  });
 
   function handleInputChange(event) {
     const target = event.target;
     const name = target.name;
     const value = target.value;
+    let selectedModels = [];
 
-    setState(state => {
-      return {
-        ...state,
-        [name]: value
-      };
-    });
+    if (target.selectedOptions && target.selectedOptions.length > 0) {
+        selectedModels = Array.from(target.selectedOptions, option => option.value);
+    }
+    
+    setJobs(jobs => ({
+      ...jobs,
+      models: selectedModels
+    }));
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    var url = "http://localhost:7181/api/Jobs";
-      async function post() {
-        const response = await fetch(url, {
-          method: 'POST',
-          body: JSON.stringify(state),
-          credentials: 'include',
-          headers: {
-            'Authorization': 'Bearer ' + localStorage.getItem("token"),
-            'Content-Type': 'application/json'
-          },
-        })
+    var url = "http://localhost:7181/api/jobs";
+    console.log(jobs)
+    async function post() {
+      const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(jobs),
+        credentials: 'include',
+        headers: {
+          'Authorization': 'Bearer ' + localStorage.getItem("token"),
+          'Content-Type': 'application/json'
+        },
+      });
 
-        if(response.ok)
-        {
-          //setMessage("Job created successfully!");
-        }
-        else 
-        {
-          //setMessage("Failed to create job");
-        }
-      };
-      
-      post(); 
+      if (response.ok) {
+        console.log("Job created successfully!");
+      } else {
+        console.log("Failed to create job");
+      }
+    };
+
+    post();
   }
 
   useEffect(() => {
-      var url = "http://localhost:7181/api/Models";
-        async function get() {
-          const response = await fetch(url, {
-            method: 'GET',
-            credentials: 'include',
-            headers: {
-              'Authorization': 'Bearer ' + localStorage.getItem("token"),
-              'Content-Type': 'application/json'
-            },
-          })
-  
-          if(response.ok)
-          {
-            const data = await response.json();
-            setModels(data);
-          }
-        };
-        
-        get(); 
+    var url = "http://localhost:7181/api/Models";
+    async function get() {
+      const response = await fetch(url, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Authorization': 'Bearer ' + localStorage.getItem("token"),
+          'Content-Type': 'application/json'
+        },
+      })
+
+      if (response.ok) {
+        const models = await response.json();
+        setModels(models);
+      }
+    };
+
+    get();
   }, []);
-    
-    return (
-      <Layout>
-          <Fragment>
+
+  return (
+    <Layout>
+      <Fragment>
         <h2> Jobs </h2>
-        
+
         <div className="react-form">
-        <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit}>
             <div className="input-div">
               <label> Customer </label>
               <input name="customer" placeholder="Customer" autoFocus='true' onChange={handleInputChange} />
@@ -89,7 +96,6 @@ export function Jobs() {
               <label > Days </label>
               <input name="days" placeholder="Days" onChange={handleInputChange} />
             </div>
-          
 
             <div className="input-div">
               <label > Location </label>
@@ -103,23 +109,22 @@ export function Jobs() {
 
             <div className="input-div">
               <label>Model</label>
-              <select name="model">
-                <option value="">Select a Model</option>
-                {models && models.map(model => (
-                  <option key={model.efModelId} value={model.efModelId}>
-                    {model.efModelId} {model.firstName} {model.lastName}
-                  </option>
-                ))}
-              </select>
+              <select name="models" multiple onChange={handleInputChange}>
+  {models && models.map(model => (
+    <option key={model.efModelId} value={model.efModelId}>
+      {model.efModelId} {model.firstName} {model.lastName}
+    </option>
+  ))}
+</select>
             </div>
 
             <div className="button-container">
               <button className="submit-button"> Create new job </button>
             </div>
 
-        </form>
+          </form>
         </div>
       </Fragment>
-      </Layout>
-    )
+    </Layout>
+  )
 }
